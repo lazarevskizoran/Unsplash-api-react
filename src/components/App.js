c276d1eb90cba6b19440a48dcf0f7210a07ef16d
lazarevskizoran <100445665+lazarevskizoran@users.noreply.github.com>
2022-11-23 11:34:19 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -22,6 +22,10 @@ class App extends React.Component {
       <div className='ui container' style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
         <ImageList images={this.state.images} />
+        <h2 class='ui center aligned icon header'>
+          <i class='images icon'></i>
+          Search on Unsplash
+        </h2>
       </div>
     );
   }
